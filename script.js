81e16a3cc8047650167aa044b330bb50abfefdfd
Giuseppe <giuseppegiaccio@yahoo.de>
2025-03-23 15:24:48 +0100

    Cambio testo pulsante in base al tema attivo

---
+++ script.js
@@ -0,0 +1,23 @@
+const darkButton = document.getElementById("toggle-dark");
+const body = document.body
+function updateButtonText() {
+    if (body.classList.contains("dark-mode")) {
+        darkButton.textContent = "Light mode"
+    } else {
+        darkButton.textContent = "Dark mode"
+    }
+}
+darkButton.addEventListener("click", function () {
+    body.classList.toggle("dark-mode");
+    if (body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light")
+    }
+    updateButtonText()
+})
+if (localStorage.getItem("theme") === "dark") {
+    body.classList.add("dark-mode")
+}
+updateButtonText()
+
